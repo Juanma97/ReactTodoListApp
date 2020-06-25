c53a9ca04b2efa3e97c1af1d9abc65fd50dff93f
Juan Manuel Perez Toro <juanma97@outlook.com>
2020-06-25 17:21:06 +0100

    Delete todo implemented

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import Title from './Title';
-import Input from './Input';
-
 function App() {
+
+  const state = useState([]);
+  const todos = state[0];
+  const setTodos = state[1];
+
+  const addItem = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  }
+
+  function editTodo() {
+    
+  }
+
+  function deleteTodo (todoToDelete){
+    var index = todos.indexOf(todoToDelete);
+    todos.splice(index, 1);
+    setTodos([...todos]);
+  };
+
+  useEffect(() => {
+    console.log("Cambio de estado");
+  })
+
   return (
     <div className="App">
-      <Title />
-      <Input />
-      <div>
-        <p>Todo 1</p>
-        <p>Todo 2</p>
-        <p>Todo 3</p>
-        <p>Todo 4</p>
+      <h1>TodoList App</h1>
+      <input type="text" id="input-todo"></input>
+      <button onClick={() => {addItem(document.getElementById("input-todo").value)}}>Add Todo</button>
+      <div className="todos-container">
+        {todos.map(todo => (
+          <div className="todo">
+            <p key="todo">{todo}</p>
+            <div className="edit-todo" onClick={() => editTodo()}>Edit</div>
+            <div className="delete-todo" onClick={() => deleteTodo(todo)}>Delete</div>
+          </div>
+        ))}
       </div>
     </div>
   );
